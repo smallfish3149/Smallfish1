5916ef50ea76c02351e47fb987b49fded2f324ce
smallfish <1062303733@qq.com>
2021-05-26 13:52:31 +0800

    Commit!

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -2,7 +2,5 @@ public class git {
     public static void main(String[] args) {
         System.out.println("smallfish");
         System.out.println("smallfish");
-        String name ="smallfish";
-        int age = 26;
     }
 }
